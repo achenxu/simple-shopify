c445ec52abfbd9001079e56820711448e9baaa98
Matt <gr8bitmail@gmail.com>
2018-01-13 19:18:32 -0600

    Proxy Support
    - Proxy Support
    - Minor bugs fixed

--- monitor.js
+++ monitor.js
@@ -3,6 +3,7 @@ try {
     var request = require("request");
     var cheerio = require("cheerio");
     var Webhook = require("webhook-discord")
+    var fs = require("fs");
 
 
  
@@ -12,6 +13,22 @@ try {
     var Hook = new Webhook(config.webhook.url)
     var cookiejar = request.jar();
     var soldoutMap = new Map()
+    var text = fs.readFileSync("./proxies.txt", "utf-8");
+
+    var textByLine = text.split("\n")
+    var formattedProxies = new Array();
+
+
+    for(i=0; i<textByLine.length;i++) {
+        var splitthing = textByLine[i].split(":");
+        if(splitthing.length > 1) {
+            formattedProxies.push("http://" + splitthing[2] + ":" + splitthing[3] + "@" + splitthing[0] + ":" + splitthing[1])
+        } else {
+            formattedProxies.push("http://" + splitthing[0] + ":" + splitthing[1])
+        }
+        
+    }
+    console.log(" --- SIMPLE PROXY MADE BY LUQY / MATT C. @isn_name --- ")
 
     for(i = 0; i < urls.length; i++) {
         //soldoutobj[getHostName(urls[i])] = false;
@@ -30,6 +47,15 @@ try {
         return (i < 10) ? "0" + i : i;
     }
 
+    function pickRandomProxy() {
+        if(config.useproxy) {
+            var rand = Math.floor(Math.random() * formattedProxies.length) + 0;
+            console.log("[INFO] Using proxy " + formattedProxies[rand])
+            return formattedProxies[rand];
+        } else {
+            return ""
+        }
+    }
     
     
     function monitor(urllink) {
@@ -42,9 +68,11 @@ try {
                 "Upgrade-Insecure-Requests": "1",
                 "Cache-Control": "max-age=0",
             },
+            proxy: pickRandomProxy(),
             jar: cookiejar
         }, function(error, response, body) {
                     if (!error && response.statusCode == 200) {
+                        
                 var today = new Date(),
                 h = checkTime(today.getHours()),
                 m = checkTime(today.getMinutes()),
@@ -53,15 +81,14 @@ try {
                 var $ = cheerio.load(body);
                 if ($("table.stock-problems-table > tbody > tr > td:nth-child(3)").text().trim() == "Sold out") {
                     
-                    console.log("---------")
                     console.log("[INFO] Monitored website. " + getHostName(urllink) + " is out of stock at " + time)
-                    console.log("---------")
+                
                     //soldout = false;
                     soldoutMap.set(getHostName(urllink), "0")
                 } else if ($("button.step__footer__continue-btn btn > span").text().trim() == "Continue to payment method" || "Continue to shipping method") {
-                    console.log("---------")
+                    
                     console.log("[INFO] Monitored website. " + getHostName(urllink) + " is in stock. at " + time )
-                    console.log("---------")
+                    
                     var price = $("table.product-table > tbody > tr > td.product__price > span.order-summary__emphasis").text().trim()
                     // console.log("DEBUH - " + getHostName(urllink))
                     // console.log("DEBUG - " + soldoutMap.get(getHostName(urllink)))
@@ -78,17 +105,20 @@ try {
                 } else {
                     console.log("[INFO] Monitored website. Unknown item status.")
                 }
-            } else {
-                console.log("[ERROR] There was an error monitoring the website: " + error.message)
+                
+            } else {    
+                console.log("[ERROR] There was an error monitoring the website: " + error)
             }
         });
         
     }
     function checkMonitor() {
         for(i = 0; i < urls.length; i++) {
+            
             monitor(urls[i])
+            
         }
-        setTimeout(arguments.callee, 5000);
+        setTimeout(arguments.callee, config.interval);
     }   
     checkMonitor()
     function getHostName(url) {
